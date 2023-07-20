05729732ba2d7e1cceb17e54e36c9ab3ca94c62f
Reddimus <Kevi.M.CECS@outlook.com>
2023-07-19 17:04:20 -0700

    Added C++ file

---
+++ Binary_Search/Silver_P1_2016-Counting_Haybales/Silver_P1_2016-Counting_Haybales.cpp
@@ -0,0 +1,43 @@
+/*
+Counting Haybales - USACO Silver P1 2016 prompt:
+
+Farmer John has just arranged his N haybales (1≤N≤100,000) at various points along 
+the one-dimensional road running across his farm. To make sure they are spaced out 
+appropriately, please help him answer Q queries (1≤Q≤100,000), each asking for the 
+number of haybales within a specific interval along the road.
+
+INPUT FORMAT (file haybales.in):
+The first line contains N and Q.
+
+The next line contains N distinct integers, each in the range 0…1,000,000,000, 
+indicating that there is a haybale at each of those locations.
+
+Each of the next Q lines contains two integers A and B (0≤A≤B≤1,000,000,000) 
+giving a query for the number of haybales between A and B, inclusive.
+
+OUTPUT FORMAT (file haybales.out):
+You should write Q lines of output. For each query, output the number of haybales 
+in its respective interval.
+
+SAMPLE INPUT:
+4 6
+3 2 7 5
+2 3
+2 4
+2 5
+2 7
+4 6
+8 10
+
+SAMPLE OUTPUT:
+2
+2
+3
+4
+1
+0
+*/
+
+int main(){
+    
+}
