8cfe310b3929c2905fe67f2925aeb9ba87a0c728
Reddimus <Kevi.M.CECS@outlook.com>
2023-07-27 20:38:05 -0700

    Added Python Solution

--- Binary_Search/Silver_P1_2017-Cow_Dance_Show/Silver_P1_2017-Cow_Dance_Show.py
+++ Binary_Search/Silver_P1_2017-Cow_Dance_Show/Silver_P1_2017-Cow_Dance_Show.py
@@ -27,9 +27,9 @@ value is an integer in the range 1…100,000.
 It is guaranteed that if K=N, the show will finish in time.
 
 OUTPUT FORMAT (file cowdance.out):
-Print out the smallest possible value of K
- such that the dance performance will take no more than Tmax
- units of time.
+Print out the smallest possible value of K such that the dance performance will take no more 
+than Tmax units of time.
+
 SAMPLE INPUT:
 5 8
 4
@@ -37,6 +37,104 @@ SAMPLE INPUT:
 8
 6
 4
+
+In the sample input provided, the time each cow dances for is specified as follows:
+
+- Cow 1 dances for 4 units of time
+- Cow 2 dances for 7 units of time
+- Cow 3 dances for 8 units of time
+- Cow 4 dances for 6 units of time
+- Cow 5 dances for 4 units of time
+
+t_max = 8
+
+initialized
+[4, 7, 8, 6] | len(stage) = 4 | t = 0
+
+[0, 3, 4, 2] | len(stage) = 4 | t = 4
+added cow 5 in idx 0
+[4, 3, 4, 2] | len(stage) = 4 | t = 4
+
+Dance remaining time
+[0, 0, 0, 0] | len(stage) = 4 | t = 8
+
 SAMPLE OUTPUT:
 4
+'''
+
+# Binary Search - Heap approach
+# T: O(n * log n), M: O(n), where n is number of cows
+
+import heapq
+
+# open input file
+with open("cowdance.in") as r:
+    # first line n = cows, t_max = maximum time for all cows to dance
+    n, t_max = map(int, r.readline().split())
+    # for the next n lines read each cow's dance time
+    dances = [int(r.readline()) for ln in range(n)]
+
+# k represents the size of the stage to support n amount of cows dancing
+def within_contract(k):
+    stage = dances[:k]
+    heapq.heapify(stage)
+    # elapse (heappop) shortest dance time and add next cow to dance
+    for d_idx in range(k, n):
+        min_dance = heapq.heappop(stage)
+        heapq.heappush(stage, min_dance + dances[d_idx])
+    return max(stage) <= t_max
+
+# Search for smallest possible stage size within contract limit using binary search
+lo, hi = 1, n
+while lo < hi:
+    mid = (lo + hi) // 2
+    if not within_contract(k = mid):
+        lo = mid + 1    # increase stage size to reduce time on stage and be within contract time limit
+    else:
+        hi = mid
+
+# Write searched item into output file
+print(hi, file=open("cowdance.out", "w"))
+
+'''
+# Binary Search - Array approach
+# T: O(n^2 * log n), M: O(n), where n is number of cows
+
+# open input file
+with open("cowdance.in") as r:
+    # first line n = cows, t_max = maximum time for all cows to dance
+    n, t_max = map(int, r.readline().split())
+    # for the next n lines read each cow's dance time
+    dances = [int(r.readline()) for ln in range(n)]
+
+# k represents the size of the stage to support n amount of cows dancing
+def within_contract(k):
+    stage = dances[:k]
+    t_cnt = 0
+    d_idx = k
+    while d_idx < n:
+        min_dance = min(stage)
+        t_cnt += min_dance
+        if t_cnt > t_max:
+            return False
+        # elapse time by shortest dance time to open dance spot and add next cow to dance
+        for s_idx, s_time in enumerate(stage):
+            if s_time != min_dance:
+                stage[s_idx] -= min_dance
+            else:
+                stage[s_idx] = dances[d_idx]
+                d_idx += 1
+    # let cows dance remaining time
+    return t_cnt + max(stage) <= t_max
+
+# Search for smallest possible stage size within contract limit using binary search
+lo, hi = 1, n
+while lo < hi:
+    mid = (lo + hi) // 2
+    if not within_contract(k = mid):
+        lo = mid + 1    # increase stage size to reduce time on stage and be within contract time limit
+    else:
+        hi = mid
+# Write searched item into output file
+print(hi, file=open("cowdance.out", "w"))
 '''
