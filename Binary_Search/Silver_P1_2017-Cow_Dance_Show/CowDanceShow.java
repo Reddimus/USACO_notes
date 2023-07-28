1da40434aab5dd43d5d5f1daf1bd93af3d9b0d62
Reddimus <Kevi.M.CECS@outlook.com>
2023-07-28 00:23:16 -0700

    Added Priority Queue approach

--- Binary_Search/Silver_P1_2017-Cow_Dance_Show/CowDanceShow.java
+++ Binary_Search/Silver_P1_2017-Cow_Dance_Show/CowDanceShow.java
@@ -27,9 +27,9 @@ value is an integer in the range 1…100,000.
 It is guaranteed that if K=N, the show will finish in time.
 
 OUTPUT FORMAT (file cowdance.out):
-Print out the smallest possible value of K
- such that the dance performance will take no more than Tmax
- units of time.
+Print out the smallest possible value of K such that the dance performance will take no more 
+than Tmax units of time.
+
 SAMPLE INPUT:
 5 8
 4
@@ -37,10 +37,126 @@ SAMPLE INPUT:
 8
 6
 4
+
 SAMPLE OUTPUT:
 4
 */
 
+import java.io.*;
+import java.util.*;
+
+// Binary search - PriorityQueue approach
+// T: O(n log n), M: O(n), where n is the number of cows
+
 public class CowDanceShow {
-    
+    // Make vals accessible by all methods
+    private static int n, t_max;
+    private static List<Integer> dances = new ArrayList<Integer>();
+    // k represents the size of the stage to support n amount of cows dancing
+    private static boolean within_contract(int k) {
+        PriorityQueue<Integer> stage = new PriorityQueue<Integer>(dances.subList(0, k));
+        // elapse shortest dance time to open dance spot and add next cow to stage
+        for (int idx = k; idx < n; idx++) {
+            int min_dance = stage.poll();
+            stage.add(min_dance + dances.get(idx));
+        }
+        // find the longest dance time in the stage then dance remaining time
+        int max_dance = Integer.MIN_VALUE;
+        for (int i = 0; i < k; i++)
+            max_dance = Math.max(max_dance, stage.poll());
+        return max_dance <= t_max;
+    }
+    public static void main(String[] args) throws IOException {
+        // open input file
+        BufferedReader in = new BufferedReader(new FileReader("cowdance.in"));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        // first line: n = number of cows, tmax = max stage time
+        n = Integer.parseInt(st.nextToken());
+        t_max = Integer.parseInt(st.nextToken());
+        // for the next n lines read each cow's dance time
+        for (int i = 0; i < n; i++)
+            dances.add(Integer.parseInt(in.readLine()));
+
+        // Binary Search for smallest possible stage size within contract time limit
+        int lo = 1, hi = n;
+        while (lo < hi) {
+            int mid  = lo + (hi - lo) / 2;
+            if (!within_contract(mid))
+                lo = mid + 1;
+            else
+                hi = mid;
+        }
+
+        // Write output file
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("cowdance.out")));
+        out.println(hi);
+        out.close();
+    }
 }
+
+/*
+// Binary search - Array approach
+// T: O(n^2 log n), M: O(n), where n is the number of cows
+
+public class CowDanceShow {
+    // make vals accessible by all methods
+    private static int[] dances;
+    private static int n, tmax;
+
+    // k represents the size of the stage to support n amount of cows dancing
+    private static boolean within_contract(int k) {
+        int[] stage = Arrays.copyOfRange(dances, 0, k);
+        int t_cnt = 0, d_idx = k;
+        while (d_idx < n) {
+            // find the shortest dance time in the stage
+            int min_dance = Integer.MAX_VALUE;
+            for (int i = 0; i < k; i++)
+                min_dance = Math.min(min_dance, stage[i]);
+            t_cnt += min_dance;
+            if (t_cnt > tmax)
+                return false;
+            // elapse time by shortest dance time to open dance spot and add next cow to stage
+            for (int i = 0; i < k; i++) {
+                stage[i] -= min_dance;
+                if (stage[i] == 0) {
+                    stage[i] = dances[d_idx];
+                    d_idx++;
+                }
+            }
+        }
+        int max_dance = Integer.MIN_VALUE;
+        for (int i = 0; i < k; i++)
+            max_dance = Math.max(max_dance, stage[i]);        
+        return t_cnt + max_dance <= tmax;
+    }
+
+    public static void main(String[] args) throws Exception{
+        // open input file
+        BufferedReader in = new BufferedReader(new FileReader("cowdance.in"));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        // first line: n = number of cows, tmax = max stage time
+        n = Integer.parseInt(st.nextToken()); 
+        tmax = Integer.parseInt(st.nextToken());
+        // for the next n lines read each cow's dance time
+        dances = new int[n];
+        for (int i = 0; i < n; i++)
+            dances[i] = Integer.parseInt(in.readLine());
+        in.close();
+        
+        // Binary Search for smallest possible stage size within contract time limit
+        int lo = 1, hi = n;
+        while (lo < hi) {
+            int mid  = lo + (hi - lo) / 2;
+            if (!within_contract(mid))
+                lo = mid + 1;
+            else
+                hi = mid;
+        }
+
+        // Write output file
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("cowdance.out")));
+        out.println(hi);
+        out.close();
+    }
+}
+*/
