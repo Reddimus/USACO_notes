1e2b54e02f6aea440cff9ba2c90ff9a64458b006
Reddimus <Kevi.M.CECS@outlook.com>
2023-08-01 00:45:11 -0700

    Removed print statements

--- Binary_Search/Silver_P1_2018-Convention/Silver_P1_2018-Convention.py
+++ Binary_Search/Silver_P1_2018-Convention/Silver_P1_2018-Convention.py
@@ -48,20 +48,18 @@ with open('convention.in', 'r') as f:
     n, m, c = map(int, f.readline().split())
     # Second line: cow arrival times
     arrivals = list(map(int, f.readline().split()))
-    print(n, m, c)
-    print(arrivals)
 
 # Sort cow arrival times to group cows that arrive close together
 arrivals.sort()
 
-def valid_waiting(max_waiting: int) -> bool:
+def valid_waiting(max_wait: int) -> bool:
     buses = cow_Lidx = cow_Ridx = 0
     while cow_Ridx < n:
         # if first cow in bus (overlapping indexes); increment buses counted
         if cow_Lidx == cow_Ridx:
             buses += 1
         # if bus waiting time out of range; update prev idx to curr idx (move to next bus)
-        if arrivals[cow_Ridx] - arrivals[cow_Lidx] > max_waiting:
+        if arrivals[cow_Ridx] - arrivals[cow_Lidx] > max_wait:
             cow_Lidx = cow_Ridx
         # if bus capacity (c) reached with new placement; update idxs to move on to next bus
         elif cow_Ridx - cow_Lidx + 1 == c:
@@ -76,7 +74,7 @@ def valid_waiting(max_waiting: int) -> bool:
 lo, hi = 0, arrivals[-1] - arrivals[0]
 while lo < hi:
     mid = (lo + hi) // 2
-    if not valid_waiting(max_waiting = mid):
+    if not valid_waiting(max_wait = mid):
         lo = mid + 1
     else:
         hi = mid
