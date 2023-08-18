63d619977ed60a082c154d51df26981cc18e961e
Reddimus <Kevi.M.CECS@outlook.com>
2023-08-18 15:27:41 -0700

    Changed path

---
+++ Binary_Search/Silver/P1_2020-Social_Distancing/P1_2020-Social_Distancing.py
@@ -0,0 +1,88 @@
+'''
+USACO 2020 US Open Contest, Silver
+Problem 1. Social Distancing
+
+Farmer John is worried for the health of his cows after an outbreak of the highly contagious 
+bovine disease COWVID-19.
+
+In order to limit transmission of the disease, Farmer John's N cows (2≤N≤10^5) have decided 
+to practice "social distancing" and spread themselves out across the farm. The farm is shaped 
+like a 1D number line, with M mutually-disjoint intervals (1≤M≤10^5) in which there is grass 
+for grazing. The cows want to locate themselves at distinct integer points, each covered in 
+grass, so as to maximize the value of D, where D represents the distance between the closest 
+pair of cows. Please help the cows determine the largest possible value of D.
+
+INPUT FORMAT (file socdist.in):
+The first line of input contains N and M. The next M lines each describe an interval in terms
+of two integers a and b, where 0≤a≤b≤10^18. No two intervals overlap or touch at their endpoints. 
+A cow standing on the endpoint of an interval counts as standing on grass.
+
+OUTPUT FORMAT (file socdist.out):
+Print the largest possible value of D such that all pairs of cows are D units apart. A solution 
+with D>0 is guaranteed to exist.
+
+SAMPLE INPUT:
+5 3
+0 2
+4 7
+9 9
+
+SAMPLE OUTPUT:
+2
+
+One way to achieve D=2 is to have cows at positions 0, 2, 4, 6 and 9.
+
+SCORING:
+- Test cases 2-3 satisfy b≤10^5.
+- Test cases 4-10 satisfy no additional constraints.
+'''
+
+# Binary search on multiple answers method
+# T: O(N log D + M log M) = O((N+M)log(maxDist)), M: O(M), where N is the number of cows, M is the number of intervals, and D is the range of distance
+
+# Read input file
+with open('socdist.in', 'r') as f:
+    # read the number of cows and grassy areas from the first line
+    cows, grassy_areas = map(int, f.readline().split())
+    intervals = []
+    # Read each interval pair and store them
+    for interval in range(grassy_areas):
+        start, end = map(int, f.readline().split())
+        intervals.append((start, end))
+
+# sort so we can read location of each grass area chronologically
+intervals.sort()
+
+# Function to test if all cows can be placed maintaining a certain minimum distance
+def possible_placement(min_dist: int) -> bool:
+    remaining_cows = cows
+    prev_cow_loc = intervals[0][0] - min_dist
+    # iterating through each sorted interval allows us to find cow placements faster
+    for start, end in intervals:
+        # if next cow placement falls out of grassy area interval, prepare cow placement for while loop
+        if prev_cow_loc + min_dist < start:
+            prev_cow_loc = start - min_dist
+        # maximize cow placement in current grassy area
+        while start <= prev_cow_loc + min_dist <= end:
+            prev_cow_loc += min_dist
+            remaining_cows -= 1
+            if remaining_cows <= 0:
+                return True
+    return False
+
+# Binary search multiple answers
+# worst case = 1, best case = equally distant cows
+# 1 <= result <= equally distant cows
+lo = 1
+hi = int((intervals[-1][1] - intervals[0][0]) / (cows - 1))
+# Converge to a max feasible min distance
+while (lo < hi):
+    mid = (lo + hi + 1) // 2
+    # if min_dist/mid does fit in grassy areas we save lo and try for a larger minimum distance
+    if possible_placement(mid):
+        lo = mid
+    # else min_dist/mid does not fit therefore we get rid of curr hi by lowering upper bound
+    else:
+        hi = mid - 1
+# Write minimum distance of optimally distant cows to output file
+print(lo, file=open('socdist.out', 'w'))
