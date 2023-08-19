b5d182b7b775f8a33a12532798e574371abc8c08
Reddimus <Kevi.M.CECS@outlook.com>
2023-08-18 20:21:55 -0700

    Renamed variables

--- Binary_Search/Silver/P1_2018-Convention/Convention.java
+++ Binary_Search/Silver/P1_2018-Convention/Convention.java
@@ -29,38 +29,38 @@ public class Convention {
         int lo = 0, hi = arrivals[n - 1] - arrivals[0];
         while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
-            if (!valid_waiting(mid))
+            if (!validMaxWait(mid))
                 lo = mid + 1;
             else
                 hi = mid;
         }
 
         // Write converged answer hi [..false, false, TRUE, true...] representing min max arrival time
-        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("convention.out")));
+        PrintWriter out = new PrintWriter("convention.out");
         out.println(hi);
         out.close();
     }
 
     // check if max waiting time is valid under busses (m) and bus capacity (c) constraints
-    private static boolean valid_waiting(int max_wait) {
-        int buses = 0, cowRidx = 0, cowLidx = 0;
-        while (cowRidx < n) {
+    private static boolean validMaxWait(int maxWait) {
+        int buses = 0, l_idx = 0, r_idx = 0;
+        while (r_idx < n) {
             // if first cow in bus (overlapping idxs); increment buses counter
-            if (cowLidx == cowRidx) 
+            if (l_idx == r_idx) 
                 buses++;
             
             // if bus waiting time out of range; update prev idx to curr idx (move to next bus)
-            if (arrivals[cowRidx] - arrivals[cowLidx] > max_wait) {
-                cowLidx = cowRidx;
+            if (arrivals[r_idx] - arrivals[l_idx] > maxWait) {
+                l_idx = r_idx;
             }
             // else if bus capacity (c) reached with new placement; update idxs to move on to next bus
-            else if (cowRidx + 1 - cowLidx == c) {
-                cowRidx++;
-                cowLidx = cowRidx;
+            else if (r_idx + 1 - l_idx == c) {
+                r_idx++;
+                l_idx = r_idx;
             }
             // else cow time is valid for bus placement
             else {
-                cowRidx++;
+                r_idx++;
             }
         }
         return buses <= m;
