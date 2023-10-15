b63580359b3cdf46d19935b04939ef9de83d9e3e
Reddimus <Kevi.M.CECS@outlook.com>
2023-10-14 22:00:19 -0700

    Modified comments

--- Multi-D_Dynamic_Programming/Gold/P2_2016-Circular_Barn_Revisited/P2_2016-Circular_Barn_Revisited.cpp
+++ Multi-D_Dynamic_Programming/Gold/P2_2016-Circular_Barn_Revisited/P2_2016-Circular_Barn_Revisited.cpp
@@ -1,8 +1,9 @@
-// 2-D Dynamic Programming + Deque 
-// T: O(n^3 * k), M: O(k * n)
-// Where n = number of rooms, k = number of doors to unlock
-
 #include <bits/stdc++.h>
+// #include <iostream>
+// #include <vector>
+// #include <deque>
+// #include <algorithm>
+// #include <climits>
 
 #define ll long long
 
@@ -13,7 +14,7 @@ int main() {
 	// Read first line: n = rooms, k = unlocked exterior doors
 	int n, k;
 	cin >> n >> k;
-	// For the next n lines, read the number of cows in each room (r_i)
+	// For the next n lines, read in the desired number of cows in each room (r_i)
 	deque<int> rooms(n);
 	for (int &r : rooms)
 		cin >> r;
@@ -25,57 +26,53 @@ int main() {
 	// cout << endl << endl;
 
 	ll minDist = INT64_MAX;
-	// Iterate through all possible starting rooms
+	// Rotate through all the starting position of the rooms for each dp table
 	for (int startPos = 0; startPos < n; ++startPos) {
 		vector<vector<ll>> dp(k + 1, vector<ll>(n + 1, INT64_MAX));
-		// With no door used, the distance is 0 if there is no room filled
-		dp[0][n] = 0;
-		// Iterate through number of doors used
-		for (int usedDoor = 1; usedDoor <= k; ++usedDoor) {
+		dp[0][n] = 0;   // 0 doors unlocked base case
+		// Iterate through all possible number of doors unlocked
+		for (int unlockedDoors = 1; unlockedDoors <= k; ++unlockedDoors) {
 			// Iterate through all possible positions to place this new door
 			for (int newDoorPos = 0; newDoorPos < n; ++newDoorPos) {
-				if (usedDoor >= k && newDoorPos >= 1)
+				if (unlockedDoors >= k && newDoorPos >= 1)  // If dp[k][0] is done calculating
 					break;
-				// partialDist stores the sum of distance to fill rooms [i, j-1]
+				// Calculate distance for rooms between new and last door
 				ll partialDist = 0;
-				// Iterate through all possible positions to place the last door
-				// Find the minimum distance if we use this placement with our new door at i
+				// Loop through last door positions
 				for (int lastDoorPos = newDoorPos + 1; lastDoorPos <= n; ++lastDoorPos) {
-					// Add the amount of distance needed to fill the new room at j-1
+					// Add distance needed to fill the new room at lastDoorPos - 1
 					partialDist += rooms[lastDoorPos - 1] * (lastDoorPos - newDoorPos - 1);
-					ll newDist = dp[usedDoor - 1][lastDoorPos];
+					ll newDist = dp[unlockedDoors - 1][lastDoorPos];
 					if (newDist < INT64_MAX)
 						newDist += partialDist;
-					dp[usedDoor][newDoorPos] = min(dp[usedDoor][newDoorPos], newDist);
+					dp[unlockedDoors][newDoorPos] = min(dp[unlockedDoors][newDoorPos], newDist);
 
 					// // print dp table
-					// cout << "dp table at startPos = " << startPos << ", usedDoor = " << usedDoor << ", newDoorPos = " << newDoorPos << ", lastDoorPos = " << lastDoorPos << endl;
+					// cout << "dp table at startPos = " << startPos << ", unlockedDoors = " << unlockedDoors << ", newDoorPos = " << newDoorPos << ", lastDoorPos = " << lastDoorPos << endl;
 					// for (int DoorPos = 0; DoorPos <= n; ++DoorPos)
 					// 	cout << "DoorPos " << DoorPos << "	";
 					// cout << endl;
-					// for (int usedDoor = 0; usedDoor <= k; ++usedDoor) {
-					// 	cout << "usedDoor = " << usedDoor << ": ";
+					// for (int unlockedDoors = 0; unlockedDoors <= k; ++unlockedDoors) {
+					// 	cout << "unlockedDoors = " << unlockedDoors << ": ";
 					// 	for (int newDoorPos = 0; newDoorPos <= n; ++newDoorPos) {
-					// 		if (dp[usedDoor][newDoorPos] == INT64_MAX)
+					// 		if (dp[unlockedDoors][newDoorPos] == INT64_MAX)
 					// 			cout << "inf" << " ";
 					// 		else
-					// 			cout << dp[usedDoor][newDoorPos] << " ";
+					// 			cout << dp[unlockedDoors][newDoorPos] << " ";
 					// 	}
 					// 	cout << endl;
 					// }
 					// cout << endl;
 				}
-
 			}
 		}
-		// Update the best answer using the current dp answer
+		// Update the best answer using the current dp with current starting position
 		minDist = min(minDist, dp[k][0]);
-		// Put the first room to the end of the deque so that the first door would be placed at the second room
-		int firstRoom = rooms.front();
+		// Rotate first room to end
+		rooms.push_back(rooms.front());
 		rooms.pop_front();
-		rooms.push_back(firstRoom);
 
-		// cout << "minDist = min(minDist, dp[" << k << "][0]) = " << minDist << endl;
+		// cout << "minDist = min(minDist, dp[" << k << "][0]) = min(" << minDist << ", " << dp[k][0] << ") = " << min(minDist, dp[k][0]) << endl;
 		// // print rooms deque
 		// cout << "rooms deque: ";
 		// for (int r : rooms)
@@ -83,6 +80,7 @@ int main() {
 		// cout << endl << endl;
 	}
 
+	// Write the minDist accross all starting positions, with all possible doors unlocked
 	freopen("cbarn2.out", "w", stdout);
 	cout << minDist << endl;
 }
