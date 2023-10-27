a680b8fad9b7084b51ae5d608053571f19363e13
Reddimus <Kevi.M.CECS@outlook.com>
2023-10-26 20:45:03 -0700

    Read in input file

--- Multi-D_Dynamic_Programming/Gold/P2_2016-Cow_Checklist/CowChecklist.java
+++ Multi-D_Dynamic_Programming/Gold/P2_2016-Cow_Checklist/CowChecklist.java
@@ -3,6 +3,10 @@ import java.util.*;
 
 public class CowChecklist {
 	public static void main(String[] args) throws IOException {
-
+		BufferedReader in = new BufferedReader(new FileReader("checklist.in"));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		int h = Integer.parseInt(st.nextToken());
+		int g = Integer.parseInt(st.nextToken());
+		
 	}
 }
