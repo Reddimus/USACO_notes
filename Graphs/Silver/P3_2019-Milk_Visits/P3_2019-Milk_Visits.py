c9f864daa39a5dea9944f750035b4e38221cc69a
Reddimus <Kevi.M.CECS@outlook.com>
2023-12-24 01:11:57 -0800

    Added python solution

--- Graphs/Silver/P3_2019-Milk_Visits/P3_2019-Milk_Visits.py
+++ Graphs/Silver/P3_2019-Milk_Visits/P3_2019-Milk_Visits.py
@@ -1,29 +1,55 @@
+# Breadth First Search (BFS) w/ Connected Components detection Graph approach
+# T & M: O(n + m) | O(V + E), where n = num of farms, m = num of friends
 
-with open('milkvisits.in', 'r') as f:
-	# First line: n = num of farms, m = FJ's friends
-	n, m  = map(int, f.readline().split())
+import collections
+
+with open("milkvisits.in", "r") as f:
+	# Read in First line: n = num of farms, m = num of FJ's friends
+	n, m = map(int, f.readline().split())
+
 	# Second line: string of farm types
-	farm_types = f.readline()
+	milk_types = " " + f.readline()
+	
 	# For the next n-1 lines read in the edges of the graph
-	roads = {}
+	farms = [[] for idx in range(n + 1)]
 	for ln in range(n-1):
 		x, y = map(int, f.readline().split())
-		roads[x] = roads.get(x, []) + [y]
-		roads[y] = roads.get(y, []) + [x]
+		farms[x].append(y)
+		farms[y].append(x)
 
-	def satisfies(start: int, end: int, prefered: str) -> bool:
-		# DFS path from start to end
-		path = []
-		visited = set()
-		def dfs() -> bool:
+	# Process the tree & detect the different components
+	components = [-1] * (n + 1)
+	for farm in range(1, n + 1):
+		# Don't process a farm if it's been visited already
+		if components[farm] != -1:
+			continue
 
-	# For the next m lines read & check friend's path satisfies them
-	satisfied = ""
+		# BFS to find all the farms in the same subtree
+		type = milk_types[farm]
+		q = collections.deque([farm])
+		while q:
+			qdFarm = q.popleft()
+
+			# Mark the queue'd farm as same path as current component (farm)
+			components[qdFarm] = farm
+
+			# Visit a neighbor if it's new & is of the same type
+			for neighbor in farms[qdFarm]:
+				if milk_types[neighbor] == type and components[neighbor] == -1:
+					q.append(neighbor) 
+
+	# For the next m lines read & check if friend's path satisfies them
+	ans = ""
 	for ln in range(m):
-		x, y, prefered = f.readline().split()
-		if satisfies(int(x), int(y), prefered):
-			satisfied += '1'
-		else:
-			satisfied += '0'
+		start, end, preference = f.readline().split()
 
-print(satisfied, file=open("milkvisits.out", "w"))
+		single_type = components[int(start)] == components[int(end)]
+		same_preference = milk_types[int(start)] == preference
+
+		if single_type and not same_preference:
+			ans += '0'
+		else:
+			ans += '1'
+
+# Write satisfaction of each friend in sting form to output file
+print(ans, file=open("milkvisits.out", "w"))
--- Graphs/Silver/P3_2019-Milk_Visits/README.md
+++ Graphs/Silver/P3_2019-Milk_Visits/README.md
@@ -59,6 +59,58 @@ Where `n` is number of farms, `m` is number farmer John's friends, `V` is number
 
 ### Python Code
 ```python
+import collections
+
+with open("milkvisits.in", "r") as f:
+	# Read in First line: n = num of farms, m = num of FJ's friends
+	n, m = map(int, f.readline().split())
+
+	# Second line: string of farm types
+	milk_types = " " + f.readline()
+	
+	# For the next n-1 lines read in the edges of the graph
+	farms = [[] for idx in range(n + 1)]
+	for ln in range(n-1):
+		x, y = map(int, f.readline().split())
+		farms[x].append(y)
+		farms[y].append(x)
+
+	# Process the tree & detect the different components
+	components = [-1] * (n + 1)
+	for farm in range(1, n + 1):
+		# Don't process a farm if it's been visited already
+		if components[farm] != -1:
+			continue
+
+		# BFS to find all the farms in the same subtree
+		type = milk_types[farm]
+		q = collections.deque([farm])
+		while q:
+			qdFarm = q.popleft()
+
+			# Mark the queue'd farm as same path as current component (farm)
+			components[qdFarm] = farm
+
+			# Visit a neighbor if it's new & is of the same type
+			for neighbor in farms[qdFarm]:
+				if milk_types[neighbor] == type and components[neighbor] == -1:
+					q.append(neighbor) 
+
+	# For the next m lines read & check if friend's path satisfies them
+	ans = ""
+	for ln in range(m):
+		start, end, preference = f.readline().split()
+
+		single_type = components[int(start)] == components[int(end)]
+		same_preference = milk_types[int(start)] == preference
+
+		if single_type and not same_preference:
+			ans += '0'
+		else:
+			ans += '1'
+
+# Write satisfaction of each friend in sting form to output file
+print(ans, file=open("milkvisits.out", "w"))
 ```
 
 ### C++ Code:
