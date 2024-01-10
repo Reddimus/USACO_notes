2ae47f3e52f778e6bbc0bf4a22467684920c4af0
Reddimus <Kevi.M.CECS@outlook.com>
2024-01-09 17:37:43 -0800

    Read in first line

--- Graphs/Silver/P1_2015-Switching_on_the_Lights/SwitchingLights.java
+++ Graphs/Silver/P1_2015-Switching_on_the_Lights/SwitchingLights.java
@@ -4,5 +4,11 @@ import java.util.*;
 public class SwitchingLights {
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new FileReader("lightson.in"));
+		// First line: n = # of rooms, m = # of switches
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		// For the next m lines
+		
 	}
 }
