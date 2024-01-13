674a8db8b6a32336e1c09f9bab83eed9690f08ab
Reddimus <Kevi.M.CECS@outlook.com>
2024-01-13 00:08:35 -0800

    Read input

--- Graphs/Silver/P3_2016-Build_Gates/BuildGates.java
+++ Graphs/Silver/P3_2016-Build_Gates/BuildGates.java
@@ -3,6 +3,12 @@ import java.util.*;
 
 public class BuildGates {
 	public static void main(String[] args) throws IOException {
-		
+		BufferedReader in = new BufferedReader(new FileReader("gates.in"));
+		int n = Integer.parseInt(in.readLine());
+		boolean[][] isFence = new boolean[2*n+1][2*n+1];
+		int x = n, y = n;
+		for (int i = 0; i < n; ++i) {
+			StringTokenizer st = new StringTokenizer(in.readLine());
+		}
 	}
 }
