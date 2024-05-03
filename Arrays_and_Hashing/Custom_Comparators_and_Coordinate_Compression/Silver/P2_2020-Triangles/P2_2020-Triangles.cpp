6b9d48f9c4b16cd114d90b7a39aacec85a34c022
Reddimus <Kevi.M.CECS@outlook.com>
2024-05-03 06:35:02 -0700

    Added comments

--- Arrays_and_Hashing/Custom_Comparators_and_Coordinate_Compression/Silver/P2_2020-Triangles/P2_2020-Triangles.cpp
+++ Arrays_and_Hashing/Custom_Comparators_and_Coordinate_Compression/Silver/P2_2020-Triangles/P2_2020-Triangles.cpp
@@ -14,7 +14,7 @@ int main() {
 	// Read in first line: n = fence posts
 	int n;
 	cin >> n;
-	// For the next n lines, read in the x and y coordinates of the fence posts
+	// For the next n lines, read in the x & y coordinates into coordinate intercept maps
 	unordered_map<int, vector<pair<int, int>>> xcoords, ycoords;
 	for (int i = 0; i < n; ++i) {
 		int x, y;
@@ -26,36 +26,34 @@ int main() {
 
 	Fence fences[N_MAX];
 
-	for (auto& [x, vec] : xcoords) {
+	for (auto& [x, xintercepts] : xcoords) {
 		long long curr = 0;
 		// sort all y positions of all points w/ the same x pos
-		sort(vec.begin(), vec.end());
-		/*
-		* then we compute the value s_1 of this set:
-		* the sum of the heights of all the triangles that
-		* have their anchor point at (i, xcoords[i][0].first)
-		*/
-		for (int j = 1; j < vec.size(); j++) 
-			curr += vec[j].first - vec[0].first;
-		fences[vec[0].second].heightSum = curr;
-		for (int j = 1; j < vec.size(); j++) {
-			curr += (2 * j - vec.size()) *
-					(vec[j].first - vec[j - 1].first);
-			fences[vec[j].second].heightSum = curr;
+		sort(xintercepts.begin(), xintercepts.end());
+		// calculate the sum of heights for the first fence post
+		for (int j = 1; j < xintercepts.size(); j++) 
+			curr += xintercepts[j].first - xintercepts[0].first;
+		fences[xintercepts[0].second].heightSum = curr;
+		// calculate the sum of heights for the rest of the fence posts
+		for (int j = 1; j < xintercepts.size(); j++) {
+			curr += (2 * j - xintercepts.size()) * (xintercepts[j].first - xintercepts[j - 1].first);
+			fences[xintercepts[j].second].heightSum = curr;
 		}
 	}
 
-	// we do the sums of bases in exactly the same way
-	for (auto& [y, vec] : ycoords) {
+	// Same algorithm as above, but for y coordinates & base sum
+	for (auto& [y, yintercepts] : ycoords) {
 		long long curr = 0;
-		sort(vec.begin(), vec.end());
-		for (int j = 1; j < vec.size(); j++) 
-			curr += vec[j].first - vec[0].first;
-		fences[vec[0].second].baseSum = curr;
-		for (int j = 1; j < vec.size(); j++) {
-			curr += (2 * j - vec.size()) *
-					(vec[j].first - vec[j - 1].first);
-			fences[vec[j].second].baseSum = curr;
+		// sort all x positions of all points w/ the same y pos
+		sort(yintercepts.begin(), yintercepts.end());
+		// calculate the sum of bases for the first fence post
+		for (int j = 1; j < yintercepts.size(); j++) 
+			curr += yintercepts[j].first - yintercepts[0].first;
+		fences[yintercepts[0].second].baseSum = curr;
+		// calculate the sum of bases for the rest of the fence posts
+		for (int j = 1; j < yintercepts.size(); j++) {
+			curr += (2 * j - yintercepts.size()) * (yintercepts[j].first - yintercepts[j - 1].first);
+			fences[yintercepts[j].second].baseSum = curr;
 		}
 	}
 
