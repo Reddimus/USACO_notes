6d7d57b841b92bc6cace5ce088a06bef0e41d4e5
Reddimus <Kevi.M.CECS@outlook.com>
2024-05-17 06:27:49 -0700

    Read in first line of input

---
+++ Two_Pointers/Silver/P2_2016-Diamond_Collector/P2_2016-Diamond_Collector.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#define N_MAX 50000
+#define K_MAX 1000000000
+
+using namespace std;
+
+int main() {
+    freopen("diamond.in", "r", stdin);
+    int n, k;
+    cin >> n >> k;
+
+    return 0;
+}
