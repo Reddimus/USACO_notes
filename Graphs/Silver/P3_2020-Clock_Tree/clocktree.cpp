317f49759e097d32792f18200f3ef03ca6acf56a
Reddimus <Kevi.M.CECS@outlook.com>
2024-12-13 17:41:09 -0800

    Refactor DFS implementation for bipartite graph approach and improve variable naming

--- Graphs/Silver/P3_2020-Clock_Tree/clocktree.cpp
+++ Graphs/Silver/P3_2020-Clock_Tree/clocktree.cpp
@@ -1,4 +1,4 @@
-// DFS - Graph approach
+// Depth-first search Biparte - Graph approach
 // T: O(V + E) ≈ O(n), S: O(1)
 // Where V = n = # of rooms, E = # of corridors and clocks/adjacency list is not counted as extra space
 
@@ -25,23 +25,24 @@ int main() {
 		adjList[b].push_back(a);
 	}
 
-	int sum0 = 0, sum1 = 0, nodes0 = 0, nodes1 = 0;
-	function<void(int, int, int)> dfs = [&](int room, int color, int parent) {
-		// Update color/sum
-		if (color == 0) {
-			nodes0++;
+	// Depth-first search group 0 and group 1 nodes and their sums
+	int sum0 = 0, sum1 = 0, nodeCount0 = 0, nodeCount1 = 0;
+	function<void(int, int, int)> dfs = [&](int room, bool group, int parent) {
+		// Update sums and node counts based on the group
+		if (group == 0) {
 			sum0 += clocks[room];
+			nodeCount0++;
 		} else {
-			nodes1++;
 			sum1 += clocks[room];
+			nodeCount1++;
 		}
 
 		for (int nxtRoom : adjList[room])
-			if (nxtRoom != parent)
-				dfs(nxtRoom, 1 - color, room);	// Swap colors for the child node
+			if (nxtRoom != parent)	// if next room has not been visited
+				dfs(nxtRoom, !group, room);	// Swap groups for next room
 	};
 
-	dfs(1, 0, -1);	// Start from room 1, color 0
+	dfs(1, 0, -1);	// Start from room 1, group 0, no parent
 
 	freopen("clocktree.out", "w", stdout);
 	// Same mod means you can start anywhere
@@ -49,10 +50,10 @@ int main() {
 		cout << n << endl;
 	// if group0 is 1 smaller than group1, we must start from group1
 	else if ((sum0 + 1) % 12 == sum1 % 12)
-		cout << nodes1 << endl;
+		cout << nodeCount1 << endl;
 	// if group1 is 1 smaller than group0, we must start from group0
 	else if (sum0 % 12 == (sum1 + 1) % 12)
-		cout << nodes0 << endl;
+		cout << nodeCount0 << endl;
 	// no way to get all clocks pointing to 12
 	else
 		cout << 0 << endl;
