d798aecce09f173bebb99b7af2dae81ab0dbda19
Reddimus <Kevi.M.CECS@outlook.com>
2024-12-16 18:58:35 -0800

    Added python file

---
+++ Simulation/Bronze/P2_2015-Speeding_Ticket/speeding.py
@@ -0,0 +1,25 @@
+with open("speeding.in", "r") as fin:
+    # Read first line: n = road segments, m = bessie's segments
+    n, m = map(int, fin.readline().split())
+    # For the next n lines read in road segments
+    speed_limits = [0] * 100
+    last_position = 0
+    for ln in range(n):
+        length, speed_limit = map(int, fin.readline().split())
+        # For each mile in the road assign the speed limit using last position -> last position + length
+        for mile in range(last_position, last_position + length):
+            speed_limits[mile] = speed_limit
+
+        # Update last position
+        last_position += length
+
+    # For the next m lines read in bessie's segments
+    bessie_speeds = [0] * 100
+    last_position = 0
+    for ln in range(m):
+        length, bessie_speed = map(int, fin.readline().split())
+        for mile in range(last_position, last_position + length):
+            bessie_speeds[mile] = bessie_speed
+
+        last_position += length
+
