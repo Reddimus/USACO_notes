1fe9fe6c32385ec153880e580fa618091d8458a8
Reddimus <Kevi.M.CECS@outlook.com>
2025-01-02 19:38:33 -0800

    Refactor DaisyChains.java to use final variable for flower count and improve code readability

--- Arrays_and_Hashing/Complete_Search/Bronze/P2_2020-Daisy_Chains/DaisyChains.java
+++ Arrays_and_Hashing/Complete_Search/Bronze/P2_2020-Daisy_Chains/DaisyChains.java
@@ -5,24 +5,27 @@ public class DaisyChains {
 	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
 		// Read in 1st line: n = # of flowers
-		int n = sc.nextInt();
+		final int N = sc.nextInt();
 		// Read in 2nd line: the petals of each flower
-		int[] flowerPetals = new int[n];
-		for (int i = 0; i < n; i++)
+		int[] flowerPetals = new int[N];
+		for (int i = 0; i < N; i++)
 			flowerPetals[i] = sc.nextInt();
 		sc.close();
 
-		int avgFlowerPicCount = n;
-		for (int i = 0; i < n - 1; i++) {
+		int avgFlowerPicCount = N;
+		for (int i = 0; i < N - 1; i++) {
 			int sum = flowerPetals[i];
 
 			Map<Integer, Integer> petalCount = new HashMap<>();
 			petalCount.put(flowerPetals[i], 1);
-			for (int j = i + 1; j < n; j++) {
+			for (int j = i + 1; j < N; j++) {
 				sum += flowerPetals[j];
-				
-				petalCount.put(flowerPetals[j], petalCount.getOrDefault(flowerPetals[j], 0) + 1);
-				
+
+				petalCount.put(
+					flowerPetals[j],
+					petalCount.getOrDefault(flowerPetals[j], 0) + 1
+				);
+
 				int length = j - i + 1;
 				if (sum % length != 0)
 					continue;
