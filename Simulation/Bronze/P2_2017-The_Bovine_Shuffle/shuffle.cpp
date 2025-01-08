545f47f3d443349cac207fe3943077dbe0709dbb
Reddimus <Kevi.M.CECS@outlook.com>
2025-01-07 17:18:12 -0800

    Format code in shuffle.cpp for improved readability and consistency

--- Simulation/Bronze/P2_2017-The_Bovine_Shuffle/shuffle.cpp
+++ Simulation/Bronze/P2_2017-The_Bovine_Shuffle/shuffle.cpp
@@ -5,29 +5,29 @@
 using namespace std;
 
 int main() {
-    freopen("shuffle.in", "r", stdin);
-    // Read in first line: n = # of cows
-    int n;
-    cin >> n;
-    // Read in second line: shuffles for each cow
-    int shuffles[COW_MAX];
-    for (int cow = 1; cow <= n; cow++)
-        cin >> shuffles[cow];
-    // Read in third line: initial cow ids
-    vector<int> ids(n + 1);
-    for (int cow = 1; cow <= n; cow++)
-        cin >> ids[cow];
-    
-    for (int i = 0; i < SHUFFLE_NUM; i++) {
-        vector<int> pastOrder(n + 1);
-        for (int cow = 1; cow <= n; cow++)
-            pastOrder[cow] = ids[shuffles[cow]];
-        
-        ids = pastOrder;
-    }
+	freopen("shuffle.in", "r", stdin);
+	// Read in first line: n = # of cows
+	int n;
+	cin >> n;
+	// Read in second line: shuffles for each cow
+	int shuffles[COW_MAX];
+	for (int cow = 1; cow <= n; cow++)
+		cin >> shuffles[cow];
+	// Read in third line: initial cow ids
+	vector<int> ids(n + 1);
+	for (int cow = 1; cow <= n; cow++)
+		cin >> ids[cow];
 
-    freopen("shuffle.out", "w", stdout);
-    for (int cow = 1; cow <= n; cow++)
-        cout << ids[cow] << endl;
-    return 0;
+	for (int i = 0; i < SHUFFLE_NUM; i++) {
+		vector<int> pastOrder(n + 1);
+		for (int cow = 1; cow <= n; cow++)
+			pastOrder[cow] = ids[shuffles[cow]];
+
+		ids = pastOrder;
+	}
+
+	freopen("shuffle.out", "w", stdout);
+	for (int cow = 1; cow <= n; cow++)
+		cout << ids[cow] << endl;
+	return 0;
 }
