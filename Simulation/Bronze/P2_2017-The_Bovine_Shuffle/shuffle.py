605c29577cb4fa8aa5dfef4e018adaaa9b87f0e7
Reddimus <Kevi.M.CECS@outlook.com>
2025-01-13 17:40:04 -0800

    Add shuffle.py to implement cow shuffling logic

---
+++ Simulation/Bronze/P2_2017-The_Bovine_Shuffle/shuffle.py
@@ -0,0 +1,19 @@
+SHUFFLE_NUM = 3
+
+with open("shuffle.in", "r") as fin:
+    # Read in first line: n = # of cows
+    N = int(fin.readline().strip())
+    # Read in second line: shuffles for each cow
+    shuffles = list(map(int, fin.readline().split()))
+    # Read in third line: initial cow ids
+    ids = list(map(int, fin.readline().split()))
+
+for _ in range(SHUFFLE_NUM):
+    past_order = ids.copy()  # Save the current order before updating
+    for i in range(N):
+        ids[i] = past_order[shuffles[i] - 1]  # Adjust for 0-based indexing
+
+# Write the result to the output file
+with open("shuffle.out", "w") as fout:
+    for cow_id in ids:
+        fout.write(f"{cow_id}\n")
