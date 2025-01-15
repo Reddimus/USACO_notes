f58a545d3eb4f377e08199415c8e99a2718a17c5
Reddimus <Kevi.M.CECS@outlook.com>
2025-01-14 18:12:05 -0800

    Add cbarn.cpp to implement cow barn unlocking simulation

---
+++ Simulation/Bronze/P2_2016-Circular_Barn/cbarn.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define MAX_ROOMS 1001
+
+using namespace std;
+
+int main() {
+	freopen("cbarn.in", "r", stdin);
+	// Read in total number of rooms
+	int n;
+	cin >> n;
+	// Read in the number of cows in each room
+	int rooms[MAX_ROOMS];
+	for (int i = 0; i < n; i++)
+		cin >> rooms[i];
+
+	// Try each room as the first room to unlock
+	int min_dist = INT_MAX;
+	for (int unlock = 0; unlock < n; unlock++) {
+		int dist = 0;
+		for (int r = 0; r < n; r++) 
+			dist += rooms[(unlock + r) % n] * r;
+		min_dist = min(min_dist, dist);
+	}
+
+	freopen("cbarn.out", "w", stdout);
+	cout << min_dist << endl;
+	return 0;
+}
