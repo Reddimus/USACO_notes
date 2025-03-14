d33819c47b36228f1cdea225cba39966932ee216
Reddimus <Kevi.M.CECS@outlook.com>
2025-03-13 18:09:19 -0700

    Fix variable naming for clarity in Daisy Chains solution

--- Arrays_and_Hashing/Complete_Search/Bronze/P2_2020-Daisy_Chains/daisychains.py
+++ Arrays_and_Hashing/Complete_Search/Bronze/P2_2020-Daisy_Chains/daisychains.py
@@ -5,19 +5,19 @@ flower_petals = list(map(int, input().split()))
 
 avg_count = N
 for i in range(N - 1):
-    sum = flower_petals[i]
+    current_sum = flower_petals[i]
 
     petal_count = [0] * 1000
     petal_count[flower_petals[i]] += 1
     for j in range(i + 1, N):
-        sum += flower_petals[j]
+        current_sum += flower_petals[j]
         petal_count[flower_petals[j]] += 1
 
         length = j - i + 1
-        if sum % length != 0:
+        if current_sum % length != 0:
             continue
 
-        if petal_count[sum // length] > 0:
+        if petal_count[current_sum // length] > 0:
             avg_count += 1
 
 print(avg_count)
